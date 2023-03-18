fa294ba6b8d10124e9b0dd076b03cc0b4557b9c6
sy4ph <nopointinn@gmail.com>
2023-03-19 00:20:21 +0300

    stuff

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ rest_client = aiobungie.RESTPool(
     "EV0u7SOIM6qTK39tw9Y6wU3ffGkf5rkqZEytqRXbe8I",
     43401,
 )
+client = aiobungie.Client("457a0f8c468a45b3926d70b4b14f601c")
 
 
 @app.route("/")
 def index():
-    if not (flask.session.get("access_token")):
-        return flask.redirect(flask.url_for("login"))
-    else:
-        return flask.redirect(flask.url_for("profile"))
+    return flask.render_template(
+        "first.html",
+        auth_link=flask.url_for("login"),
+        prof_link=flask.url_for("profile"),
+        access_token=token_got,
+    )
 
 
 @app.route("/login")
@@ -51,34 +54,65 @@ async def callback():
     return flask.redirect(flask.url_for("profile"))
 
 
+# function to client fetch character
+async def get_character(
+    membership_id, membership_type, character_ids, components, num
+) -> aiobungie.crate.CharacterComponent:
+    return await client.fetch_character(
+        membership_id,
+        membership_type,
+        character_ids[num],
+        components=components,
+    )
+
+
 @app.route("/profile")
 async def profile():
     if not (flask.session.get("access_token")):
         print("redirected")
-        flask.redirect(flask.url_for("login"))
+        return flask.redirect("/")
+    else:
+        async with rest_client.acquire() as rest:
+            user = await rest.fetch_current_user_memberships(
+                flask.session["access_token"]
+            )
+        membership_id = user.get("destinyMemberships")[0].get("membershipId")
+        membership_type = user.get("destinyMemberships")[0].get("membershipType")
 
-    async with rest_client.acquire() as rest:
-        user = await rest.fetch_current_user_memberships(flask.session["access_token"])
-    print(user)
-    print("name kinda is:", user.get("bungieNetUser").get("uniqueName"))
-    membership_id = user.get("destinyMemberships")[0].get("membershipId")
-    membership_type = user.get("destinyMemberships")[0].get("membershipType")
-    print(membership_type)
-    print(membership_id)
-    token = flask.session.get("access_token")
-    headers = {
-        "X-API-Key": "457a0f8c468a45b3926d70b4b14f601c",
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json",
-    }
-    params = {"components": "Characters"}
-    request_url = f"https://www.bungie.net/Platform/Destiny2/{membership_type}/Profile/{membership_id}/?components=Characters"
-    response = requests.get(request_url, headers=headers)
+        token = flask.session.get("access_token")
+        headers = {
+            "X-API-Key": "457a0f8c468a45b3926d70b4b14f601c",
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+        params = {"components": "Characters"}
+        request_url = f"https://www.bungie.net/Platform/Destiny2/{membership_type}/Profile/{membership_id}/?components=Characters"
+        response = requests.get(request_url, headers=headers)
 
-    print(response.text)
-    return flask.render_template(
-        "profile.html", username=user.get("bungieNetUser").get("uniqueName")
-    )
+        # Get character IDs
+        character_ids = []
+        for character in response.json()["Response"]["characters"]["data"]:
+            character_ids.append(character)
+        components = [
+            aiobungie.ComponentType.CHARACTERS,
+            aiobungie.ComponentType.CURRENCY_LOOKUPS,
+            aiobungie.ComponentType.CHARACTER_INVENTORY,
+            aiobungie.ComponentType.CHARACTER_EQUIPMENT,
+        ]
+
+        character1 = await get_character(
+            membership_id, membership_type, character_ids, components, 0
+        )
+        character2 = await get_character(
+            membership_id, membership_type, character_ids, components, 1
+        )
+        character3 = await get_character(
+            membership_id, membership_type, character_ids, components, 2
+        )
+        print(character1.inventory)
+        return flask.render_template(
+            "profile.html", username=user.get("bungieNetUser").get("uniqueName")
+        )
 
 
 if __name__ == ("__main__"):
