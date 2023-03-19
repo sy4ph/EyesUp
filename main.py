20a2bf2e13a640fbc9ed0f3be216212a39399436
sy4ph <nopointinn@gmail.com>
2023-03-19 18:44:42 +0300

    Added character parsing, manifest download and images parsing

--- main.py
+++ main.py
@@ -6,11 +6,35 @@ import oauthlib.oauth2
 import requests_oauthlib
 import json
 import ssl
+import asyncio
 
 app = flask.Flask(__name__)
 app.secret_key = os.urandom(24)
 token_got = False
 # Generate a self-signed SSL/TLS certificate and private key
+async def manifest_check():
+    async with rest_client.acquire() as rest:
+        global manifest
+        with open("manifest_version.txt", "r+") as file:
+            actual_version = await rest.fetch_manifest_version()
+            local_version = file.read()
+            if (
+                os.path.isfile(os.path.join("C://Users/timk2/EyesUp", "manifest.json"))
+                and actual_version == local_version
+            ):
+                print("You fine.")
+            else:
+                file.close()
+                with open("manifest_version.txt", "w") as file:
+                    print("you not fine")
+                    download_status = "We are downloading shit! Wait a minute."
+                    await rest.download_json_manifest(
+                        "manifest", "C:\\Users\\timk2\\EyesUp"
+                    )
+                    # write the manifest version to the file
+                    file.write(str(await rest.fetch_manifest_version()))
+        with open("manifest.json", "r") as f:
+            manifest = json.load(f)
 
 
 # Configure Flask to use SSL/TLS
@@ -23,6 +47,9 @@ rest_client = aiobungie.RESTPool(
     43401,
 )
 client = aiobungie.Client("457a0f8c468a45b3926d70b4b14f601c")
+download_status = ""
+
+asyncio.run(manifest_check())
 
 
 @app.route("/")
@@ -31,6 +58,7 @@ def index():
         "first.html",
         auth_link=flask.url_for("login"),
         prof_link=flask.url_for("profile"),
+        download_status=download_status,
         access_token=token_got,
     )
 
@@ -54,18 +82,6 @@ async def callback():
     return flask.redirect(flask.url_for("profile"))
 
 
-# function to client fetch character
-async def get_character(
-    membership_id, membership_type, character_ids, components, num
-) -> aiobungie.crate.CharacterComponent:
-    return await client.fetch_character(
-        membership_id,
-        membership_type,
-        character_ids[num],
-        components=components,
-    )
-
-
 @app.route("/profile")
 async def profile():
     if not (flask.session.get("access_token")):
@@ -73,46 +89,77 @@ async def profile():
         return flask.redirect("/")
     else:
         async with rest_client.acquire() as rest:
+
             user = await rest.fetch_current_user_memberships(
                 flask.session["access_token"]
             )
-        membership_id = user.get("destinyMemberships")[0].get("membershipId")
-        membership_type = user.get("destinyMemberships")[0].get("membershipType")
+            membership_id = user.get("destinyMemberships")[0].get("membershipId")
+            membership_type = user.get("destinyMemberships")[0].get("membershipType")
 
-        token = flask.session.get("access_token")
-        headers = {
-            "X-API-Key": "457a0f8c468a45b3926d70b4b14f601c",
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json",
-        }
-        params = {"components": "Characters"}
-        request_url = f"https://www.bungie.net/Platform/Destiny2/{membership_type}/Profile/{membership_id}/?components=Characters"
-        response = requests.get(request_url, headers=headers)
+            token = flask.session.get("access_token")
+            headers = {
+                "X-API-Key": "457a0f8c468a45b3926d70b4b14f601c",
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json",
+            }
+            params = {"components": "Characters"}
+            request_url = f"https://www.bungie.net/Platform/Destiny2/{membership_type}/Profile/{membership_id}/?components=Characters"
+            response = requests.get(request_url, headers=headers)
 
-        # Get character IDs
-        character_ids = []
-        for character in response.json()["Response"]["characters"]["data"]:
-            character_ids.append(character)
-        components = [
-            aiobungie.ComponentType.CHARACTERS,
-            aiobungie.ComponentType.CURRENCY_LOOKUPS,
-            aiobungie.ComponentType.CHARACTER_INVENTORY,
-            aiobungie.ComponentType.CHARACTER_EQUIPMENT,
-        ]
+            # Get character IDs
+            character_ids = []
+            for character in response.json()["Response"]["characters"]["data"]:
+                character_ids.append(character)
+            components = [
+                aiobungie.ComponentType.CHARACTERS,
+                aiobungie.ComponentType.CURRENCY_LOOKUPS,
+                aiobungie.ComponentType.CHARACTER_INVENTORY,
+                aiobungie.ComponentType.CHARACTER_EQUIPMENT,
+            ]
+            character1 = await rest.fetch_character(
+                membership_id,
+                membership_type,
+                character_ids[0],
+                components,
+                flask.session.get("access_token"),
+            )
+            items = character1["inventory"]["data"]["items"]
+            item_hashes = [item["itemHash"] for item in items]
 
-        character1 = await get_character(
-            membership_id, membership_type, character_ids, components, 0
-        )
-        character2 = await get_character(
-            membership_id, membership_type, character_ids, components, 1
-        )
-        character3 = await get_character(
-            membership_id, membership_type, character_ids, components, 2
-        )
-        print(character1.inventory)
-        return flask.render_template(
-            "profile.html", username=user.get("bungieNetUser").get("uniqueName")
-        )
+            for item in item_hashes:
+                print(
+                    manifest["DestinyInventoryItemDefinition"][str(item)][
+                        "displayProperties"
+                    ]["name"]
+                )
+            # Get a list of equipped items
+            equipped_items = []
+            for item in character1["equipment"]["data"]["items"]:
+                equipped_items.append(item["itemHash"])
+            # Convert eqipped item hashes to item names
+            equipped_item_names = []
+            equipped_item_images = []
+            for item in equipped_items:
+                equipped_item_names.append(
+                    manifest["DestinyInventoryItemDefinition"][str(item)][
+                        "displayProperties"
+                    ]["name"]
+                )
+                equipped_item_images.append(
+                    "https://www.bungie.net"
+                    + manifest["DestinyInventoryItemDefinition"][str(item)][
+                        "displayProperties"
+                    ]["icon"]
+                )
+            print(equipped_item_images)
+            print(equipped_item_names)
+            data = {
+                "username": user.get("bungieNetUser").get("uniqueName"),
+                "membership_id": membership_id,
+                "equipped_items": equipped_item_names,
+                "equipped_item_images": equipped_item_images,
+            }
+            return flask.render_template("profile.html", **data)
 
 
 if __name__ == ("__main__"):
